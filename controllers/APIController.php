e17db00826e13561d62666161d7d13d8ff150ec9
AlexDays <proferna8@hotmail.com>
2021-12-03 17:06:35 -0700

    meta

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -25,30 +25,9 @@ class APIController{
 
         $response = $httpClient->post($endpoint, [ 'body' => $content ]);
         $status_code = $response->getStatusCode();
-        echo $status_code;
 
     }
 
     public static function getRequest(){
-        $client = new Client();
-        $addr = __DIR__ . '/api-altex-e88fd63ddc80.json';
-
-        // service_account_file.json is the private key that you created for your service account.
-        $client->setAuthConfig($addr);
-        $client->addScope('https://www.googleapis.com/auth/indexing');
-
-        // Get a Guzzle HTTP Client
-        $httpClient = $client->authorize();
-        $endpoint = 'https://indexing.googleapis.com/v3/urlNotifications/metadata?url=https%3A%2F%2Faltexdays.herokuapp.com%2F';
-
-        // Define contents here. The structure of the content is described in the next step.
-        $content = '{
-        "url": "https://altexdays.herokuapp.com/",
-        "type": "URL_UPDATED"
-        }';
-
-        $response = $httpClient->get($endpoint);
-        $status_code = $response->getStatusCode();
-        echo $status_code;
     }
 }
